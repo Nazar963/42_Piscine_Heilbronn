92ea57d120e6c34958a5fd48aad80e2773ecfda0
Nazar <62134123+Nazar963@users.noreply.github.com>
2022-04-06 17:32:00 +0200

    Add files via upload

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 16:42:18 by coder             #+#    #+#             */
+/*   Updated: 2022/02/14 16:42:18 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		result;
+	int		sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+/*
+int main()
+{
+    char str[] = "\t ---+---+1234ab567";
+   
+    // Function call
+    int val = ft_atoi(str);
+    printf("%d", val);
+    return 0;
+}
+*/
